10acd1274d6fed36f967dea7d3201cff09ded599
piggy-devil <muupaza@gmail.com>
2023-09-16 10:50:29 +0700

    create controller

---
+++ backend/controller/project.controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+
+//@desc Get all projects
+//@route GET /api/projects
+//@access Public
+export const getProjects = (req: Request, res: Response) => {
+  res.json({ message: "Get All Project" });
+};
+
+//@desc Create a new project
+//@route POST /api/projects
+//@access Private
+export const createProject = (req: Request, res: Response) => {
+  res.json({ message: "Create Project" });
+};
+
+//@desc Get a project by id
+//@route GET /api/projects/:id
+//@access Public
+export const getProject = (req: Request, res: Response) => {
+  res.json({ message: `Get Project ${req.params.id}` });
+};
+
+//@desc Update a project by id
+//@route PUT /api/projects/:id
+//@access Private
+export const updateProject = (req: Request, res: Response) => {
+  res.json({ message: `Update Project ${req.params.id}` });
+};
+
+//@desc Delete a project by id
+//@route DELETE /api/projects/:id
+//@access Private
+export const deleteProject = (req: Request, res: Response) => {
+  res.json({ message: `Delete Project ${req.params.id}` });
+};
--- backend/routes/project.routes.ts
+++ backend/routes/project.routes.ts
@@ -1,23 +1,17 @@
 import express from "express";
+import {
+  createProject,
+  deleteProject,
+  getProject,
+  getProjects,
+  updateProject,
+} from "../controller/project.controller";
 
 export default (router: express.Router) => {
-  router.get("/api/projects", (req, res) => {
-    res.json({ message: "Get All Project" });
-  });
+  router.get("/api/projects", getProjects).post("/api/projects", createProject);
 
-  router.post("/api/projects", (req, res) => {
-    res.json({ message: "Create Project" });
-  });
-
-  router.get("/api/projects/:id", (req, res) => {
-    res.json({ message: `Get Project ${req.params.id}` });
-  });
-
-  router.put("/api/projects/:id", (req, res) => {
-    res.json({ message: `Update Project ${req.params.id}` });
-  });
-
-  router.delete("/api/projects/:id", (req, res) => {
-    res.json({ message: `Delete Project ${req.params.id}` });
-  });
+  router
+    .get("/api/projects/:id", getProject)
+    .put("/api/projects/:id", updateProject)
+    .delete("/api/projects/:id", deleteProject);
 };
