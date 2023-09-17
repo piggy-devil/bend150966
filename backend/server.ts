f56dec6d382558af0b9305100d2b3cd38d156540
piggy-devil <muupaza@gmail.com>
2023-09-17 20:17:22 +0700

    db connection

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,11 @@ import express from 'express';
 import { PORT } from './utils/config';
 import router from './routes';
 import { errorHandler } from './middleware/errorMiddleware';
+import { connectDB } from './database/db';
+import * as Colors from 'colors.ts';
+Colors.colors('', '');
+
+connectDB();
 
 const app = express();
 
--- backend/utils/config.ts
+++ backend/utils/config.ts
@@ -1,6 +1,12 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
+const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
+
 export const PORT = process.env.PORT || 5555;
-export const NODE_ENV = process.env.NODE_ENV || "development";
+export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const MONGO_URI =
+    `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.sma5shn.mongodb.net/backend` ||
+    null;
